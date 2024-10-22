5dfa16c79270233dffb2ad8c59d3c4a7fa1e176a
4C3 C0CH3Z <ace.cochez@51tocarbonzero.com>
2024-10-22 17:17:47 +0100

    Added menu and started footprint page
    Still an issue

--- cobra-climate.py
+++ cobra-climate.py
@@ -14,22 +14,44 @@ def signin():
     passw = input("| Enter your password: ")
     print(("|" + " " * 30 + "|\n") * 1, end="")
     print("+", "-" * 30, "+")
-
     for credentials in uname and passw:
     # WHITELIST
         if uname == "acochez" and passw == "1234":
             print("Welcome, " + uname)
             print("+", "-" * 30, "+")
             menu()
+            break
     # BLACKLIST
         else:
             print("Incorrect credentials! Try again")
             signin()
             break
+        
+def footprint():
+    print("Type one of the following to view:")
+    view = input("information, calculator")
+    for select in footprint:
+        if view == "information":
+            print("-" * 30)
+            print("Information")
+            print("-" * 30)
 
 def menu():
-    print("You are now on the Cobra Climate app.")
+    print("You are now using the Cobra Climate program.")
     global cobraMenu
-    cobraMenu = input("")
+    print("-" * 30)
+    print("Type one of the following to view:")
+    cobraMenu = input("footprint")
+    for menu in cobraMenu:
+        if cobraMenu == "footprint":
+            print("-" * 30)
+            print("Footprint")
+            print("-" * 30)
+            footprint()
+            break
+        else:
+            print(cobraMenu, "is not an option. Try again")
+            menu()
+            break
 
 signin()
